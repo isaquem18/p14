de74ac9d3db2580593f4d4415449950b63ad20d3
isaque-patricio <isaquem18@gmail.com>
2022-06-18 22:24:46 -0300

    02

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import 'dotenv/config'
+import cors from 'cors';
 import { userRoutes } from './routes/users/index.js';
 
+const PORT = process.env.PORT || 3000;
+
 const server = express();
+
+server.use(cors());
 server.use(express.json());
 
 //routes 
@@ -9,4 +15,4 @@ server.use('/users', userRoutes);
 
 server.get('/', (req, res) => res.status(200).json({ message: "/ route is working." }));
  
-server.listen(3000, () => console.log('server started!'));
+server.listen(PORT, () => console.log('server started!'));
